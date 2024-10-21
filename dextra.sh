2f3c979bfc5c7834ae74c26c46098bb4d1a1e0b4
Tobi Delbruck <tobi@ini.uzh.ch>
2024-10-21 10:00:38 +0200

    fix startup script to source anaconda env from either anaconda or miniconda install, also to launch in terminal if not launched that way

--- dextra.sh
+++ dextra.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/bash -l
-eval "$(conda shell.bash hook)"
-conda activate dextra
-cd /home/dextra/Dropbox/GitHub/SensorsINI/dextra-roshambo-python
-python -m roshambo
+#read -s -p 'Press enter to continue...'
+# if the script was not launched from a terminal, restart it from a terminal
+if [[ ! -t 0 && -x /usr/bin/x-terminal-emulator ]]; then
+	echo "not started from terminal, starting again from terminal"
+	sleep 3
+	/usr/bin/x-terminal-emulator -e "bash -c \"$0 $*; read -s -p 'Press enter to continue...'\""
+	exit
+fi
+#read -s -p 'Press enter to cd to dextra...'
+
+cd ~/Dropbox/GitHub/SensorsINI/dextra-roshambo-python
+
+#read -s -p 'Press enter to set up conda...'
+source ~/anaconda3/etc/profile.d/conda.sh || source ~/miniconda3/etc/profile.d/conda.sh
+
+
+#read -s -p 'Press enter to activate dextra conda env...'
+conda activate dextra || conda activate ./.conda # activate the dextra env
+
+#read -s -p 'Press enter to start dextra...'
+
+python -m roshambo # run the demo
+sleep 10
